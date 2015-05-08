c6bc40183b76f6de60d9a0d6e630b3a7de4a881e
Jan Wambach <jobs@jan-wambach.de>
2015-05-08 13:32:21 +0200

    initial commit and also stuff.

---
+++ potentially_useless_functions.php
@@ -0,0 +1,29 @@
+<?php
+	// je-desto-beziehung #1
+	je ($ausloeser) {
+		# code...
+	} desto($resultat) {
+
+	}
+
+	// je-desto-beziehung #2
+	foreach ($ausloeser as $je => $desto) {
+		# code...
+	}
+
+	//how to do offscreen-rendering like a pro
+	if ( offscreen() ) {
+		//do jack shit.
+	}	
+
+	public static function MakeProfitFromUnderpants() {
+		if ( underpants() ) {
+			foreach ($clothing as $underpants => $profit) {
+				//magic happens here...
+			}
+		} else {
+			do($nothing);
+		}
+	};
+
+?>
